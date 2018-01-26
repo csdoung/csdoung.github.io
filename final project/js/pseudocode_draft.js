ded6910017c779c0efec495ec6b2f4e09e9f95af
CarrieDoung <carriedoung@gmail.com>
2018-01-25 21:12:44 -0800

    Final Project
    Pseudo JS

---
+++ final project/js/pseudocode_draft.js
@@ -0,0 +1,4 @@
+//1. when a user hovers over the homepage images to display information overlay
+//2. when a user clicks on the image, it'll go to the product page 
+//2. fixed navbar on top 
+//3. responsive design to smaller screen
